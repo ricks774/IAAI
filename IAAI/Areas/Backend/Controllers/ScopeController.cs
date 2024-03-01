fd4eed2136e3a60ee755e85f5e9cbba92817c590
ricks774 <ricks774@gmail.com>
2024-03-01 15:24:55 +0800

    feat: 🎸 add Scope Controller

---
+++ IAAI/Areas/Backend/Controllers/ScopeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IAAI.Models;
+
+namespace IAAI.Areas.Backend.Controllers
+{
+    public class ScopeController : Controller
+    {
+        private IAAIDBContent db = new IAAIDBContent();
+
+        // GET: Backend/Scope
+        public ActionResult Index()
+        {
+            return View(db.Businesses.ToList());
+        }
+
+        // GET: Backend/Scope/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Business business = db.Businesses.Find(id);
+            if (business == null)
+            {
+                return HttpNotFound();
+            }
+            return View(business);
+        }
+
+        // GET: Backend/Scope/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Backend/Scope/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Scope,Accreditation,Consult,Investigation,CreateDate")] Business business)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Businesses.Add(business);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(business);
+        }
+
+        // GET: Backend/Scope/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Business business = db.Businesses.Find(id);
+            if (business == null)
+            {
+                return HttpNotFound();
+            }
+            return View(business);
+        }
+
+        // POST: Backend/Scope/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Scope,Accreditation,Consult,Investigation,CreateDate")] Business business)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(business).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(business);
+        }
+
+        // GET: Backend/Scope/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Business business = db.Businesses.Find(id);
+            if (business == null)
+            {
+                return HttpNotFound();
+            }
+            return View(business);
+        }
+
+        // POST: Backend/Scope/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Business business = db.Businesses.Find(id);
+            db.Businesses.Remove(business);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
