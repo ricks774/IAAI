197da00c789df9651b1086df1ab8543a3c44be4b
ricks774 <ricks774@gmail.com>
2024-03-01 14:50:34 +0800

    feat: 🎸 add Business Model

---
+++ IAAI/Migrations/202403010640358_add Businss Model.cs
@@ -0,0 +1,30 @@
+﻿namespace IAAI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addBusinssModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Businesses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Scope = c.String(),
+                        Accreditation = c.String(),
+                        Consult = c.String(),
+                        Investigation = c.String(),
+                        CreateDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Businesses");
+        }
+    }
+}
